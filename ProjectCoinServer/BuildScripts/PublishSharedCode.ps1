715a6976fd95f51bd79e96ac6695de9c0423cb81
SEH00N <06psh0501@gmail.com>
2024-11-26 02:08:53 +0900

    same work as power shell

--- ProjectCoinServer/BuildScripts/PublishSharedCode.ps1
+++ ProjectCoinServer/BuildScripts/PublishSharedCode.ps1
@@ -1,2 +1,10 @@
 Write-Output "Copy SharedCode"
-Copy-Item -Path "./SharedCode/*" -Destination "../ProjectCoinClient/Assets/01. Scripts/SharedCode" -Recurse -Force
+
+$source_folder = "./src/SharedCode/"
+$target_folder = "../ProjectCoinClient/Assets/01. Scripts/SharedCode/"
+
+# 대상 폴더 삭제
+Remove-Item -Path $target_folder -Recurse -Force
+
+# 폴더 복사
+Copy-Item -Path $source_folder -Destination $target_folder -Recurse -Force
