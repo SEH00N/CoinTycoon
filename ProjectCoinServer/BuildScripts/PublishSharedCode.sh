b97cc48ad8e30e76440c86754d216c7d4099a0a2
SEH00N <06psh0501@gmail.com>
2024-12-14 07:15:04 +0900

    build script modify

--- ProjectCoinServer/BuildScripts/PublishSharedCode.sh
+++ ProjectCoinServer/BuildScripts/PublishSharedCode.sh
@@ -3,5 +3,5 @@
 echo "Copy SharedCode"
 
 source_folder="./src/SharedCode/"
-target_folder="../ProjectCoinClient/Assets/01. Scripts/SharedCode/"
+target_folder="../ProjectCoinClient/Assets/01.Scripts/SharedCode/"
 cp -rf "$source_folder" "$target_folder"
