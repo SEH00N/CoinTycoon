ba4828d34ca452f2749abd3dcc025ee1ca646211
SEH00N <06psh0501@gmail.com>
2024-12-14 14:33:01 +0900

    depth write

--- ProjectCoinClient/Assets/01.Scripts/System/Farm/Farmer/FSM/Actions/FarmerMoveAction.cs
+++ ProjectCoinClient/Assets/01.Scripts/System/Farm/Farmer/FSM/Actions/FarmerMoveAction.cs
@@ -11,6 +11,7 @@ namespace ProjectCoin.Farms.AI
 
             Vector3 targetPosition = aiData.currentTarget.position;
             Vector3 direction = targetPosition - brain.transform.position;
+            direction.z = 0f;
 
             float moveSpeed = Time.deltaTime * aiData.farmerStat[EStatType.MoveSpeed];
             Vector3 moveVector = direction.normalized * moveSpeed;
