6cb4a4ca037c80f4b6e2824564682e8bd5d8d34c
SEH00N <06psh0501@gmail.com>
2024-12-21 23:53:49 +0900

    add navmesh

--- ProjectCoinClient/Assets/01.Scripts/System/Unit/UnitMovement.cs
+++ ProjectCoinClient/Assets/01.Scripts/System/Unit/UnitMovement.cs
@@ -1,45 +1,49 @@
-using H00N.Extensions;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace ProjectCoin.Units
 {
     public class UnitMovement : MonoBehaviour
     {
-        [SerializeField] float acceleration = 0f;
-        private float maxSpeed = 0f;
-        private float velocity = 0f;
-        private Vector2 currentDestination = Vector2.zero;
+        private NavMeshAgent navAgent = null;
 
-        private void FixedUpdate()
+        private void Awake()
         {
-            Vector3 currentPosition = transform.position;
-            Vector3 directionVector = currentDestination - currentPosition.PlaneVector();
-            if(directionVector.sqrMagnitude <= 0.1f)
-            {
-                velocity = 0f;   
-                return;
-            }
-
-            velocity += acceleration * Time.fixedDeltaTime;
-            velocity = Mathf.Min(velocity, maxSpeed);
-            
-            Vector3 direction = directionVector.normalized;
-            transform.position += direction * (velocity * Time.fixedDeltaTime);
+            navAgent = GetComponent<NavMeshAgent>();
+            navAgent.updateRotation = false;
+            navAgent.updateUpAxis = false;
         }
+
+        // private void FixedUpdate()
+        // {
+        //     Vector3 currentPosition = transform.position;
+        //     Vector3 directionVector = currentDestination - currentPosition.PlaneVector();
+        //     if(directionVector.sqrMagnitude <= 0.1f)
+        //     {
+        //         velocity = 0f;   
+        //         return;
+        //     }
+
+        //     velocity += acceleration * Time.fixedDeltaTime;
+        //     velocity = Mathf.Min(velocity, maxSpeed);
+            
+        //     Vector3 direction = directionVector.normalized;
+        //     transform.position += direction * (velocity * Time.fixedDeltaTime);
+        // }
 
         public void SetMaxSpeed(float maxSpeed)
         {
-            this.maxSpeed = maxSpeed;
+            navAgent.speed = maxSpeed;
         }
 
         public void SetAcceleration(float acceleration)
         {
-            this.acceleration = acceleration;
+            navAgent.acceleration = acceleration;
         }
 
         public void SetDestination(Vector2 destination)
         {
-            currentDestination = destination;
+            navAgent.SetDestination(destination);
         }
     }
 }
